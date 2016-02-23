4087c85ebb34442a75ee14dc314c18c13add59be
tony-luisi <t.luisi@gmail.com>
2016-02-23 17:34:11 +1300

    updated twitter

--- quotegenerator.js
+++ quotegenerator.js
@@ -28,6 +28,23 @@ function load() {
     return false;
   });
 
+	window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+ 
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+ 
+  return t;
+}(document, "script", "twitter-wjs"));
+
 	!function(d,s,id) {
 		var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
 		if(!d.getElementById(id)){
